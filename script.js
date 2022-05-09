eab04460927862eccdc146f173772ba401e0b8dd
Suchita Sriramka <suchitasriramka@gmail.com>
2022-05-09 11:01:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector(".navbar");
+
+window.onscroll = () => {
+    const top = window.scrollY;
+    console.log(top);
+    if (top >= 100) {
+        nav.classList.add("backround-dark");
+    }
+    else {
+        nav.classList.remove("backround-dark");
+    }
+}
